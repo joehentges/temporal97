5a30469ad28302487b5e135b72b54c24f06bb719
Joe Hentges <joeypeterhentges@gmail.com>
2026-05-26 22:06:54 -0500

    feat: add fumadocs folder

---
+++ docs/src/lib/shared.ts
@@ -0,0 +1,11 @@
+export const appName = 'temporal97';
+export const docsRoute = '/docs';
+export const docsImageRoute = '/og/docs';
+export const docsContentRoute = '/llms.mdx/docs';
+
+// fill this with your actual GitHub info, for example:
+export const gitConfig = {
+  user: 'joehentges',
+  repo: 'temporal97',
+  branch: 'master',
+};
